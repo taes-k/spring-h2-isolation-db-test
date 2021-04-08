0394631d350f2e0d78aef93dd9a6ac602c735937
taes <taes@wemakeprice.com>
2021-04-08 10:35:01 +0900

    feature: 독립 DB 테스트 환경

---
+++ src/test/java/com/taes/kim/dbisolationtest/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.taes.kim.dbisolationtest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+public abstract class IntegrationTest
+{
+    @Autowired
+    protected MockMvc mockMvc;
+
+}
